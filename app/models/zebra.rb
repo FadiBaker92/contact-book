d24f939a02e4cc6b1e6668a20f1e12d8e0b578c1
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-06-11 14:14:32 +0000

    testing

---
+++ app/models/zebra.rb
@@ -0,0 +1,8 @@
+class Zebra < ActiveRecord::Base
+  self.table_name = "contacts"
+end
+
+#learned how to create a data base and how to connect the data base.
+#created a table with commands.
+#experimented with model class using rails console.
+#learned how to create a new record.
